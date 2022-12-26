907d0ac4e5287e4b25b18b091f8bc4522dc84678
thiagoaderaldo <thiago.lessa>
2022-12-26 14:59:57 -0300

    Modulo 03 - aula 02 - alteração de um item na página

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
+somar.addEventListener("click", (evento) => {
+  braco.value = parseInt(braco.value) + 1;
+});
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
-})
-
-function dizOi(nome) {
-    console.log('Oi ' + nome)
-    console.log('Bem-vindo ao Robotron 2000')
-}
-
-dizOi("Pedro") 
+subtrair.addEventListener("click", (evento) => {
+  braco.value = parseInt(braco.value) - 1;
+});
