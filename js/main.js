56ef3e06774eaa1085d9889cbdad40f0a1352a4f
thiagoaderaldo <thiago.lessa>
2022-12-26 13:56:12 -0300

    Módulo 02 - aula 04 - funções com javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const robotron = document.querySelector("#robotron");
 
-robotron.addEventListener("click", dizOi);
 
-function dizOi() {
-  console.log("oi");
+robotron.addEventListener("click", (evento) => {
+    console.log(evento)
+})
+
+function dizOi(nome) {
+    console.log('Oi ' + nome)
+    console.log('Bem-vindo ao Robotron 2000')
 }
+
+dizOi("Pedro") 
